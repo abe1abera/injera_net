70d8f8bffa8c9493e9fa94f29617a503246006b7
abe1abera <abelaberab@gmail.com>
2025-10-12 22:37:06 +0300

    Set up API routing for authentication and CRUD endpoints

--- injera_net/core/urls.py
+++ injera_net/core/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import RegisterView, UserViewSet, ProductViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+router = DefaultRouter()
+router.register('users', UserViewSet, basename='user')
+router.register('products', ProductViewSet, basename='product')
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('auth/register/', RegisterView.as_view(), name='register'),
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls)),
 ]
