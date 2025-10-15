c63897bd9016888486a65f405f1c3a03b71700a7
abe1abera <abelaberab@gmail.com>
2025-10-15 22:37:18 +0300

    Add notification endpoints to URLs

--- injera_net/core/urls.py
+++ injera_net/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RegisterView, UserViewSet, ProductViewSet, OrderViewSet, PaymentViewSet, DeliveryViewSet, InventoryViewSet
+from .views import RegisterView, UserViewSet, ProductViewSet, OrderViewSet, PaymentViewSet, DeliveryViewSet, InventoryViewSet, NotificationViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
@@ -15,9 +15,13 @@ router.register('deliveries', DeliveryViewSet, basename='delivery')
 
 router.register('inventory', InventoryViewSet, basename='inventory') 
 
+router.register('notifications', NotificationViewSet, basename='notification')
+
 urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='register'),
     path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls)),
 ]
+
+
